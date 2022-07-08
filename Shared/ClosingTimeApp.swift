2d48de5d142ad38aa3c2dd2759f581b115cdf3dc
royankit11 <royankit11@gmail.com>
2022-07-08 13:18:14 -0400

    Initial Commit

---
+++ Shared/ClosingTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosingTimeApp.swift
+//  Shared
+//
+//  Created by Rik Roy on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ClosingTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
